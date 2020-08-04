227e6a90ff8ffb749f1c4d99659fb441f92b356e
chingaloEric <echingalo@gmail.com>
2020-08-04 18:35:08 +0300

    fix: 🐛 changed the api global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('soil-analysis/api');
 
   app.use(
     session({
