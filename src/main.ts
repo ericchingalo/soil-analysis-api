4d75e651d5a6b386e5199eaafb9fd9d3dd121978
chingaloEric <echingalo@gmail.com>
2020-08-05 09:27:04 +0300

    fix: 🐛 enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as session from 'express-session';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('soil-analysis/api');
 
   app.use(
